a6a584f2853eba03fa4324b1a934ab69bf6cb167
Prithviraj-Koparde <prithvirajkoparde@gmail.com>
2026-01-13 13:31:47 +0530

    created mapper class for entities and wrote code for patient mapper

--- HMS-Backend/src/main/java/com/Application/HospitalManagementSys/DataTransferObject/PatientDTO.java
+++ HMS-Backend/src/main/java/com/Application/HospitalManagementSys/DataTransferObject/PatientDTO.java
@@ -4,6 +4,7 @@ package com.Application.HospitalManagementSys.DataTransferObject;
 import com.Application.HospitalManagementSys.ENUM.Gender;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -47,7 +48,7 @@ public class PatientDTO {
     @NotBlank(message = "Contact number is required!")
     private String urgency;
 
-    @NotBlank(message = "Contact number is required!")
+    @NotNull(message = "Contact number is required!")
     @Min(value = 0, message = "Fees cannot be negative")
     private Double fees;
 }
---
+++ HMS-Backend/src/main/java/com/Application/HospitalManagementSys/Mapper/PatientMapper.java
@@ -0,0 +1,41 @@
+package com.Application.HospitalManagementSys.Mapper;
+
+import com.Application.HospitalManagementSys.DataTransferObject.PatientDTO;
+import com.Application.HospitalManagementSys.Entity.Patient;
+
+public class PatientMapper {
+    public static Patient mapToPatient(PatientDTO patientDTO) {
+
+        Patient patient = new Patient(
+                patientDTO.getId(),
+                patientDTO.getName(),
+                patientDTO.getContactNumber(),
+                patientDTO.getAge(),
+                patientDTO.getGender(),
+                patientDTO.getBloodGroup(),
+                patientDTO.getDose(),
+                patientDTO.getPrescription(),
+                patientDTO.getUrgency(),
+                patientDTO.getFees()
+        );
+
+        return patient;
+    }
+
+    public static PatientDTO mapToPatient(Patient patient) {
+
+        PatientDTO patientDTO = new PatientDTO(
+                patient.getPatientId(),
+                patient.getName(),
+                patient.getContactNumber(),
+                patient.getAge(),
+                patient.getGender(),
+                patient.getBloodGroup(),
+                patient.getDose(),
+                patient.getPrescription(),
+                patient.getUrgency(),
+                patient.getFees()
+        );
+        return patientDTO;
+    }
+}
