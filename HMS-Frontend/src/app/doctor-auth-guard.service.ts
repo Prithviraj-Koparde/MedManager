441d22e793d43567477910ba4aafaa4335aa96a9
Prithviraj-Koparde <prithvirajkoparde@gmail.com>
2026-02-08 16:04:07 +0530

    added a service class for security of doctor related routes using canActivate() interface and implementing in it

---
+++ HMS-Frontend/src/app/doctor-auth-guard.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, GuardResult, MaybeAsync, Router, RouterStateSnapshot } from '@angular/router';
+import { authService } from './auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DoctorAuthGuardService implements CanActivate {
+
+  constructor(private authService: authService, private router: Router) { }
+
+  canActivate() {
+    if (this.authService.isUserLoggedIn()) {
+
+      return true
+
+    } else {
+
+      this.router.navigate(['doc-login'])
+      return false
+
+    }
+  }
+}
