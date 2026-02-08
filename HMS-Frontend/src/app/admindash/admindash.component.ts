f324e6eb36805be64db6e722dfa4a97adf2a9e98
Prithviraj-Koparde <prithvirajkoparde@gmail.com>
2026-02-08 16:01:44 +0530

    added a method logout in admindash html fetching services from authService class

--- HMS-Frontend/src/app/admindash/admindash.component.ts
+++ HMS-Frontend/src/app/admindash/admindash.component.ts
@@ -2,8 +2,9 @@ import { Component } from '@angular/core';
 import { PatientService } from '../patient.service';
 import { Patient } from '../patient';
 import { CommonModule } from '@angular/common';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 import { FormsModule } from "@angular/forms";
+import { authService } from '../auth.service';
 
 @Component({
   selector: 'app-admindash',
@@ -13,7 +14,7 @@ import { FormsModule } from "@angular/forms";
 })
 export class AdmindashComponent {
 
-  constructor(private patientService: PatientService) { }
+  constructor(private patientService: PatientService, private authService: authService, private router:Router) { }
   ngOnInit(): void {
     this.getPatients();
   }
@@ -26,10 +27,15 @@ export class AdmindashComponent {
     })
   }
 
-  deletePatientById(id:number){
-    this.patientService.deletePatientById(id).subscribe(data=>{
+  deletePatientById(id: number) {
+    this.patientService.deletePatientById(id).subscribe(data => {
       console.log(data);
       this.getPatients();
     })
+  }
+
+  logOut() {
+    this.authService.logOut()
+    this.router.navigate(['home-page'])
   }
 }
