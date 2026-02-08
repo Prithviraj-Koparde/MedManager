58a6206f36324af72f9ea4a9788195087d3cb68f
Prithviraj-Koparde <prithvirajkoparde@gmail.com>
2026-02-08 16:06:22 +0530

    secured all paths with canActivate mapped appropriate component for each route

--- HMS-Frontend/src/app/app.routes.ts
+++ HMS-Frontend/src/app/app.routes.ts
@@ -12,20 +12,22 @@ import { ViewPatientCardComponent } from './view-patient-card/view-patient-card.
 import { UpdateMedicineComponent } from './update-medicine/update-medicine.component';
 import { DocLoginComponent } from './doc-login/doc-login.component';
 import { AdminLoginComponent } from './admin-login/admin-login.component';
+import { AdminAuthGuardService } from './admin-auth-guard.service';
+import { DoctorAuthGuardService } from './doctor-auth-guard.service';
 
 export const routes: Routes = [
-    { path: 'admin-dash', component: AdmindashComponent },
-    { path: 'appointment-dash', component: AppointmentDashComponent },
-    { path: 'add-appointment-dash', component: CreateAppointmentComponent },
+    { path: 'admin-dash', component: AdmindashComponent, canActivate: [AdminAuthGuardService] },
+    { path: 'appointment-dash', component: AppointmentDashComponent, canActivate: [AdminAuthGuardService] },
+    { path: 'add-appointment-dash', component: CreateAppointmentComponent, canActivate: [AdminAuthGuardService] },
     { path: 'home-page', component: HomePageComponent },
     { path: '', redirectTo: 'home-page', pathMatch: 'full' }, // to set the home page as default page
-    { path: 'doc-dash', component: DoctorDashComponent },
-    { path: 'add-patient-dash', component: CreatePatientComponent },
-    { path: 'med-dash', component: MedicineDashComponent },
-    { path: 'add-medicine-dash', component: AddMedicineComponent },
-    { path: 'update-patient/:id', component: UpdatePatientComponent }, // to go in patient with its id
-    { path: 'patient-card/:id', component: ViewPatientCardComponent },
-    { path: 'update-medicine/:id', component: UpdateMedicineComponent }, // to go in medicine with its id
+    { path: 'doc-dash', component: DoctorDashComponent, canActivate: [DoctorAuthGuardService] },
+    { path: 'add-patient-dash', component: CreatePatientComponent, canActivate: [DoctorAuthGuardService] },
+    { path: 'med-dash', component: MedicineDashComponent, canActivate: [DoctorAuthGuardService] },
+    { path: 'add-medicine-dash', component: AddMedicineComponent, canActivate: [DoctorAuthGuardService] },
+    { path: 'update-patient/:id', component: UpdatePatientComponent, canActivate: [DoctorAuthGuardService] }, // to go in patient with its id
+    { path: 'patient-card/:id', component: ViewPatientCardComponent, canActivate: [DoctorAuthGuardService] },
+    { path: 'update-medicine/:id', component: UpdateMedicineComponent, canActivate: [DoctorAuthGuardService] }, // to go in medicine with its id
     { path: 'doc-login', component: DocLoginComponent },
     { path: 'admin-login', component: AdminLoginComponent }
 ];
