cd6bbf199260672520749b446f8131f8670f617b
Prithviraj-Koparde <prithvirajkoparde@gmail.com>
2026-02-08 15:58:35 +0530

    added methods like isUserLoggeIn() and Logout()

--- HMS-Frontend/src/app/auth.service.ts
+++ HMS-Frontend/src/app/auth.service.ts
@@ -9,7 +9,7 @@ export class authService {
 
   docAuthenticate(username: string, password: string) {
     if (username == "prithvi" && password == "1234") {
-      sessionStorage.setItem(username, password)
+      sessionStorage.setItem('username', username)
       return true
     } else {
       return false
@@ -18,10 +18,22 @@ export class authService {
 
   adminAuthenticate(username: string, password: string) {
     if (username == "admin" && password == "1234") {
-      sessionStorage.setItem(username, password)
+      sessionStorage.setItem('username', username)
       return true
     } else {
       return false
     }
+  }
+
+  isUserLoggedIn(){
+    console.log("User logged in successfully!")
+    let user = sessionStorage.getItem('username')
+
+    return !(user==null)
+  }
+
+  logOut(){
+    console.log("User logged out")
+    sessionStorage.removeItem('username')
   }
 }
