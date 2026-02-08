9aef37cf1f2f248f2693b67685136ba262b9b399
Prithviraj-Koparde <prithvirajkoparde@gmail.com>
2026-02-08 15:57:46 +0530

    Added view patient component again for deleting it accidently

--- HMS-Frontend/src/app/view-patient-card/view-patient-card.component.spec.ts
+++ HMS-Frontend/src/app/view-patient-card/view-patient-card.component.spec.ts
@@ -3,21 +3,21 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ViewPatientCardComponent } from './view-patient-card.component';
 
 describe('ViewPatientCardComponent', () => {
-  let component: ViewPatientCardComponent;
-  let fixture: ComponentFixture<ViewPatientCardComponent>;
+    let component: ViewPatientCardComponent;
+    let fixture: ComponentFixture<ViewPatientCardComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [ViewPatientCardComponent]
-    })
-    .compileComponents();
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            imports: [ViewPatientCardComponent]
+        })
+            .compileComponents();
 
-    fixture = TestBed.createComponent(ViewPatientCardComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+        fixture = TestBed.createComponent(ViewPatientCardComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
